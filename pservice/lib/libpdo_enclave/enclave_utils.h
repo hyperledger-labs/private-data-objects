6f0ae03849df606f108ae9dd36d0bcad59bc7029
andmiele1 <36966495+andmiele1@users.noreply.github.com>
2018-07-16 14:23:42 -0700

    fixed ENCLAVE_DEBUG issue: #if -> #ifdef (#39)
    Signed-off-by: andrea miele <andrea.miele@intel.com>

--- pservice/lib/libpdo_enclave/enclave_utils.h
+++ pservice/lib/libpdo_enclave/enclave_utils.h
@@ -20,7 +20,7 @@
 #include "error.h"
 #include "parson.h"
 
-#if ENCLAVE_DEBUG
+#ifdef ENCLAVE_DEBUG
 #define SAFE_LOG(LEVEL, FMT, ...) Log(LEVEL, FMT, ##__VA_ARGS__)
 #else
 #define SAFE_LOG(LEVEL, FMT, ...)
