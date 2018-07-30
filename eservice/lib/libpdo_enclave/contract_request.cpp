04e0885b859f8e0893f4f021af3a7396ec0545a9
Andrey Brozhko <32775532+anvabr@users.noreply.github.com>
2018-07-30 23:58:26 +0100

    Fixed a typo in the assert (#54)
    Signed-off-by: anvabr <andrey.brozhko@r3.com>

--- eservice/lib/libpdo_enclave/contract_request.cpp
+++ eservice/lib/libpdo_enclave/contract_request.cpp
@@ -120,7 +120,7 @@ ContractRequest::ContractRequest(
     // contract state
     ovalue = json_object_dotget_object(request_object, "ContractState");
     pdo::error::ThrowIf<pdo::error::ValueError>(
-        !pvalue, "invalid request; failed to retrieve ContractState");
+        !ovalue, "invalid request; failed to retrieve ContractState");
 
     ByteArray id_hash = Base64EncodedStringToByteArray(contract_id_);
     pdo::error::ThrowIf<pdo::error::ValueError>(
