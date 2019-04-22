bc92cc645c833439148692a1dd70a1a26ac79c13
Mic Bowman <mic.bowman@intel.com>
2019-04-22 04:49:28 -0700

    Fix catch/throw Gipsy package for the lack of continuation support.
    Signed-off-by: Mic Bowman <mic.bowman@intel.com>

--- common/interpreter/gipsy_scheme/packages/catch-package.scm
+++ common/interpreter/gipsy_scheme/packages/catch-package.scm
@@ -27,22 +27,37 @@
      (for-each (lambda (a) (write a)) args)
      (newline))
 
-   (define (throw . x)
+   (define (throw-with-no-continuations . x)
+     (if (_more-handlers?)
+         (begin (apply (_pop-handler) x) (quit -1))
+         ;;(apply (_pop-handler) x)
+         (apply error x)))
+
+   (define (throw-with-continuations . x)
      (if (_more-handlers?)
          (apply (_pop-handler) x)
          (apply error x)))
 
-   (macro (catch form)
+   (macro (catch-with-no-continuations form)
+     `(begin (catch-throw::_push-handler ,(cadr form)) ,@(cddr form)))
+
+   (macro (catch-with-continuations form)
      (let ((label (gensym)))
        `(call/cc (lambda (exit)
                    (catch-throw::_push-handler (lambda msg (exit (apply ,(cadr form) msg))))
                    (let ((,label (begin ,@(cddr form))))
                      (catch-throw::_pop-handler)
-                     ,label)))))))
+                     ,label)))))
+   ))
 
-(define catch catch-throw::catch)
-(define throw catch-throw::throw)
 (define error-print catch-throw::error-print)
+(if (and (defined? 'call/cc) (closure? call/cc))
+    (begin
+      (define catch catch-throw::catch-with-continuations)
+      (define throw catch-throw::throw-with-continuations))
+    (begin
+      (define catch catch-throw::catch-with-no-continuations)
+      (define throw catch-throw::throw-with-no-continuations)))
 
 ;; Must compile with error hook enabled for this to work correctly
 (define *error-hook* throw)
