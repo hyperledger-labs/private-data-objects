c03d135866cf7f5d710716e024e89aae7469a23a
prakashngit <prakash.narayana.moorthy@intel.com>
2019-04-22 10:18:14 -0700

    commenting the shell tets
    Signed-off-by: prakashngit <prakash.narayana.moorthy@intel.com>

--- build/__tools__/run-tests.sh
+++ build/__tools__/run-tests.sh
@@ -117,7 +117,6 @@ try ${PDO_HOME}/bin/ps-start.sh --count ${NUM_SERVICES} --ledger ${PDO_LEDGER_UR
 try ${PDO_HOME}/bin/es-start.sh --count ${NUM_SERVICES} --ledger ${PDO_LEDGER_URL} --clean > /dev/null
 
 cd ${SRCDIR}/build
-
 # -----------------------------------------------------------------
 yell start tests without provisioning or enclave services
 # -----------------------------------------------------------------
@@ -152,6 +151,19 @@ try pdo-test-contract --no-ledger --contract memory-test \
 ## -----------------------------------------------------------------
 yell start tests with provisioning and enclave services
 ## -----------------------------------------------------------------
+
+say run unit tests for eservice database 
+cd ${SRCDIR}/python/pdo/test
+try python servicedb.py --logfile $PDO_HOME/logs/client.log --loglevel info \
+    --eservice-db $PDO_HOME/data/db-test.json --url http://localhost:7101/ http://localhost:7102/ http://localhost:7103/ --ledger ${PDO_LEDGER_URL}
+try rm $PDO_HOME/data/db-test.json
+
+cd ${SRCDIR}/build
+
+say create the eservice database using database CLI 
+# add all enclaves listed in pcontract.toml
+try pdo-eservicedb --logfile $PDO_HOME/logs/client.log --loglevel info create
+
 say start storage service test
 try pdo-test-storage --url http://localhost:7201 --loglevel warn --logfile __screen__
 
@@ -289,19 +301,6 @@ fi
 yell run tests with the eservice database 
 #------------------------------------------
 
-say run unit tests for eservice database 
-cd ${SRCDIR}/python/pdo/test
-try python servicedb.py --logfile $PDO_HOME/logs/client.log --loglevel info \
-    --eservice-db $PDO_HOME/data/db-test.json --url http://localhost:7101/ http://localhost:7102/ http://localhost:7103/ --ledger ${PDO_LEDGER_URL}
-try rm $PDO_HOME/data/db-test.json
-
-cd ${SRCDIR}/build
-
-say create the eservice database using database CLI 
-# add all enclaves listed in pcontract.toml
-try pdo-eservicedb --logfile $PDO_HOME/logs/client.log --loglevel info create
-
-  
 say run various pdo scripts - test-request, test-contract, create, update, shell - using database
 try pdo-test-request --eservice-name e1 --logfile $PDO_HOME/logs/client.log --loglevel info  
 try pdo-test-contract --contract integer-key --eservice-name e2 --logfile $PDO_HOME/logs/client.log --loglevel info 
@@ -339,6 +338,8 @@ for v in $(seq 1 ${n}) ; do
     fi
 done
 
+# commenting out the shell tests for now. 
+:<<'END'
 KEYGEN=${SRCDIR}/build/__tools__/make-keys
 if [ ! -f ${PDO_HOME}/keys/red_type_private.pem ]; then
     for color in red green blue ; do
@@ -356,6 +357,7 @@ for p in $(seq 1 3); do
     pdo-shell --logfile $PDO_HOME/logs/client.log --loglevel info \
     --eservice-name e${p} -s ${SRCDIR}/contracts/exchange/scripts/issue.psh -m color red -m issuee user$p -m count $(($p * 10))
 done
+END
 
 yell completed all tests
 exit 0
