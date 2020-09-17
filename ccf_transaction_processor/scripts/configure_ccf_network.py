36249810db93a451abfb2d0f2d81453ef08fa9d2
Mic Bowman <mic.bowman@intel.com>
2020-09-17 13:27:40 -0700

    Make the use of PDO_LEDGER_KEY_ROOT consistent
    PDO_LEGER_KEY_ROOT is used in different ways for
    CCF and Sawtooth. But there is no reason why they
    need to be in different locations. This unifies
    the use of PDO_LEDGER_KEY_ROOT. If you want the CCF
    keys in the CCF directory tree then change the
    variable when you build and install the PDO TP.
    
    Signed-off-by: Mic Bowman <mic.bowman@intel.com>

--- ccf_transaction_processor/scripts/configure_ccf_network.py
+++ ccf_transaction_processor/scripts/configure_ccf_network.py
@@ -33,7 +33,7 @@ ContractHome = os.environ.get("PDO_HOME") or os.path.realpath("/opt/pdo")
 CCF_BASE = os.environ.get("CCF_BASE")
 CCF_Bin = os.path.join(CCF_BASE, "bin")
 CCF_Etc = os.path.join(ContractHome, "ccf", "etc")
-CCF_Keys = os.path.join(ContractHome, "ccf", "keys")
+CCF_Keys = os.environ.get("PDO_LEDGER_KEY_ROOT") or os.path.join(ContractHome, "ccf", "keys")
 
 sys.path.insert(1, CCF_Bin)
 from infra.clients import CCFClient
