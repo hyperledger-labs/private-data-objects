d07f209ae245371b08094c9ec49c6cf807fc682b
Marcela Melara <marcela.melara@intel.com>
2021-03-01 17:49:34 -0800

    Bump WAMR release version to WAMR-01-29-2021
    Signed-off-by: Marcela Melara <marcela.melara@intel.com>

--- contracts/wawaka/contract-build.cmake
+++ contracts/wawaka/contract-build.cmake
@@ -60,7 +60,7 @@ STRING(REPLACE ";" " " EMCC_LINK_OPTIONS "${EMCC_LINK_OPTIONS}")
 
 # the -O2 is actually required for the moment because it removes
 # uncalled functions that clutter the wasm file
-SET(CMAKE_CXX_FLAGS "-O2 -fPIC -fno-exceptions ${EMCC_BUILD_OPTIONS}")
+SET(CMAKE_CXX_FLAGS "-O2 -fPIC -fno-exceptions -std=c++11 ${EMCC_BUILD_OPTIONS}")
 SET(CMAKE_EXECUTABLE_SUFFIX ".wasm")
 
 FILE(GLOB COMMON_SOURCE ${PDO_TOP_DIR}/contracts/wawaka/common/*.cpp)
